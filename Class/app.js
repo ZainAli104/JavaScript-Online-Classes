941424516840d204db14a72b65cba0cde1c1adc0
Zain Ali <chzainali6363@gmail.com>
2023-01-12 19:59:01 +0500

    first commit

---
+++ Class/app.js
@@ -0,0 +1,9 @@
+function addArgus() {
+    let sum = 0;
+    for (let i=0; arguments.length > i; i++) {
+        sum = sum + arguments[i];
+    }
+    console.log(sum);
+};
+
+addArgus(1, 2, 3, 4, 5, 6);
